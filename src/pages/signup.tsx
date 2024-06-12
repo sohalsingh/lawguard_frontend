80f6d30d18c43b126cbffe49e67e98e58589dc0d
Sohal Singh <singhsohal575@gmail.com>
2024-06-12 11:03:29 +0530

    seventh commit

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -10,7 +10,7 @@ const Signup = () => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        const res = await fetch('http://localhost:4000/api/auth/signup', {
+        const res = await fetch('http://localhost:3000/api/auth/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
