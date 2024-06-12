f017859617c918e006d497f3b09147fcb4cac0ec
Sohal Singh <singhsohal575@gmail.com>
2024-06-12 10:49:34 +0530

    sixth commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        const res = await fetch('http://localhost:4000/api/auth/login', {
+        const res = await fetch('http://localhost:3000/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
